0cd9fd82983b6b9f7239c51991e13a735c6c0f74
Christo <christokonidaris@gmail.com>
2021-05-27 13:03:25 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'APDS7311-Task2-ChristoKonidaris19013888';
+}
